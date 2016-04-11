f9c4a85d5bde382ae331272a40000e59433edd75
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-11 20:01:15 +0100

    First test passing

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get 'restaurants' => 'restaurants#index'
+  # get 'restaurants' => 'restaurants#index'
 
   resources :restaurants
 
