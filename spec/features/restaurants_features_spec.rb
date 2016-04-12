1f4a525e76e46023fb15f34d09171a2cf92a9fd0
Kyle Buttner <kylebuttner@gmail.com>
2016-04-12 16:16:08 +0100

    begin adding reviews

--- spec/features/restaurants_features_spec.rb
+++ spec/features/restaurants_features_spec.rb
@@ -67,3 +67,18 @@ feature 'restaurants' do
     end
   end
 end
+
+feature 'reviewing' do
+  before {Restaurant.create name: 'KFC'}
+
+  scenario 'allows uers to leave a review using a form' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content 'so so'
+  end
+end
