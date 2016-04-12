c975a005696c30f64e005f8e60a69ef4ac3f36b4
adil ali <adiln1993@gmail.com>
2016-04-12 14:02:42 +0100

    added restaurant model

--- spec/features/restaurants_features_spec.rb
+++ spec/features/restaurants_features_spec.rb
@@ -8,4 +8,16 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaraunts have been added' do
+    before do
+      Restaurant.create(name: 'Gourmet Burger Kitchen')
+    end
+
+    scenario 'display restaraunts' do
+      visit '/restaurants'
+      expect(page).to have_content('Gourmet Burger Kitchen')
+      expect(page).not_to have_content('No restaraunts yet')
+    end
+  end
 end
