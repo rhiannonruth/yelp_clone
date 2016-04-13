bfdcbbe4ba4b7ba6736ac72dc17be6515a371c8b
adil ali <adiln1993@gmail.com>
2016-04-13 12:08:49 +0100

    added dependency to restaurant

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,5 @@
 # §require 'rails_helper'
 
 describe Restaurant, type: :model do
-  it { is_expected.to have_many :reviews }
+  it { is_expected.to have_many(:reviews).dependent(:destroy)}
 end
--- spec/models/reviews_spec.rb
+++ spec/models/reviews_spec.rb
@@ -1,4 +1,4 @@
 
 describe Review, type: :model do
-  it { is_expected.to belong_to :restaurant}
+  it { is_expected.to belong_to(:restaurant)}
 end
