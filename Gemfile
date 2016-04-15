3cfdb6f84e3a553f31e2992388de1ad72f17e389
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-15 13:40:32 +0100

    Added ability to upload image for restaurant

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'haml-rails', '~> 0.9'
 gem 'devise'
 gem 'omniauth-facebook'
 gem 'dotenv'
+gem 'paperclip'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,11 @@ class Restaurant < ActiveRecord::Base
   validates :user, presence: true
   has_many :user_reviews, through: :reviews, source: :user
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  # validates :image, presence: true
+
   def build_review(review_params, user)
     review = Review.new(review_params)
     review.restaurant = self
